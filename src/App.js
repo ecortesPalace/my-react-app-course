b3aa2fdce82c139df585f293ac8729fca9fdcf80
ecortesPalace <ecortes@palaceresorts.com>
2018-08-10 18:02:18 -0500

    new function for change name

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 25},
+      { name: 'Stephanie', age: 18},
+      { name: 'Angel', age: 28}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (new_name) => {
+    //console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: new_name, age:28 },
+        { name: 'Manu', age:25 },
+        { name: 'Stephanie', age:18 },
+        { name: 'Angel', age:28 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +37,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Person name="Eduardo" age="23">My Hobbies: Run and Play Pubg</Person>
+        <button onClick={this.switchNameHandler.bind(this,'Maximilian')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this,'Max!')}>My Hobbies: Run and Play Pubg</Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}>My Hobbies: Run and Play Pubg</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>My Hobbies: Run and Play Pubg</Person>
+        <Person 
+          name={this.state.persons[3].name} 
+          age={this.state.persons[3].age}>My Hobbies: Run and Play Pubg</Person>
       </div>
     );
   }
